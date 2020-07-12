228c7a70891990a98fb86a9bf078eba17c2c9e4c
Mika Beckerich <beckerichmika@gmail.com>
2020-07-12 19:09:04 +0200

    Display prototypes

---
+++ include/display.h
@@ -0,0 +1,9 @@
+void TM1637_Init();
+void TM1637_clearScreen();
+
+void TM1637_setBrightness(int brightness);
+void TM1637_setSegments(const int segments[]);
+void TM1637_showNumber(int number);
+
+void TM1637_LEDon();
+void TM1637_LEDoff();
---
+++ src/display.c
@@ -0,0 +1,36 @@
+#include <include/defines.h>
+#include "nrf_gpio.h"
+
+void TM1637_Init() {
+    nrf_gpio_cfg_pin_input(TM1637_DIO, NRF_GPIO_PIN_PULLDOWN);
+    nrf_gpio_cfg_pin_input(TM1637_CLK, NRF_GPIO_PIN_PULLDOWN);
+}
+
+void TM1637_clearScreen(){
+
+}
+
+void TM1637_setBrightness(int brightness){
+
+}
+
+void TM1637_setSegments(const int segments[]){
+    
+}
+
+void TM1637_showNumber(int number){
+
+}
+
+void TM1637_LEDon(){
+
+}
+
+void TM1637_LEDoff() {
+
+}
+
+void TM1637_clearScreen() {
+    int data[] = {0, 0, 0, 0};
+    TM1637_setSegments(data);
+}
