bf5d96db0a81fd1dffd18e804bdff84a0912d7ee
Mika Beckerich <beckerichmika@gmail.com>
2020-07-12 19:09:23 +0200

    Definition placeholders

---
+++ include/defines.h
@@ -0,0 +1,22 @@
+//Definitions for TM1637 segments
+
+#define TM1637_ECO
+#define TM1637_DRIVE
+#define TM1637_SPORT
+#define TM1637_LIGHT
+#define TM1637_BT
+#define TM1637_TEMP
+#define TM1637_ERR
+#define TM1637_BAT100
+#define TM1637_BAT80
+#define TM1637_BAT60
+#define TM1637_BAT40
+#define TM1637_BAT20
+#define TM1637_KMH
+#define TM1637_MPH
+
+//Definitions for GPIO
+
+#define TM1637_DIO
+#define TM1637_CLK
+//There are 3 pins connected to both ports. Weird. P0.25, P0.04 and P0.05
